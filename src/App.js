0fe49d7ba07c013fa9be3206b42bba477fc7daaa
shobiianis <shoaibwork2000@gmail.com>
2023-02-23 01:11:20 +0500

    fc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [name, setName] = useState("");
+
+    
+let showName = (name) => {
+
+  console.log(name);
 }
 
+
+
+  return (
+ 
+  <div>
+   
+    
+    <input type="text" placeholder="Enter your name" onChange={(e)=>{
+     
+     setName(e.target.value);
+
+     showName(name);
+
+    }} />
+  
+  </div>
+
+    
+  );
+
+  }
+
 export default App;
+
+
+// this will print on console the input field as it changes in real time
